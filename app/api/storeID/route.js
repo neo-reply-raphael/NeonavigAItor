a9f8a601d3dac3295feb73813ecfe3d01567e219
Raphael Richter (Neo Reply) <122495465+neo-reply-raphael@users.noreply.github.com>
2024-04-15 15:56:32 +0200

    Update route.js

--- app/api/storeID/route.js
+++ app/api/storeID/route.js
@@ -8,8 +8,8 @@ export async function POST(request) {
     const req = await request.json()
     const jsonData = await fsPromises.readFile(dataFilePath);
     const objectData = JSON.parse(jsonData);
-    objectData.openAIKey = req.id
-    await fsPromises.writeFile(dataFilePath, JSON.stringify(objectData));
+    // objectData.openAIKey = req.id
+    // await fsPromises.writeFile(dataFilePath, JSON.stringify(objectData));
     return NextResponse.json(objectData)
   
 }
